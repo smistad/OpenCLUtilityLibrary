d3b5279376c4ba1a8294f20a1fd460a6472a9bc2
Janne Beate Lervik Bakeng <Janne.Beate.Lervik.Bakeng@sintef.no>
2014-01-30 15:32:39 +0100

    Added test for checking OpenGL interop capabilities.

--- testing/tests.cpp
+++ testing/tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch/catch.hpp"
 
+#include "TestFixture.hpp"
 #include "OpenCLManager.hpp"
 
 // TODO Make tests for:
@@ -9,50 +10,75 @@
 namespace test
 {
 
-TEST_CASE("OpenCLManager: Can create instance of the manager.","[OpenCLManager]"){
+
+//Helper functions
+std::vector<cl::Device> getDevices(oul::DeviceType type){
+    oul::DeviceCriteria criteria;
+    criteria.setTypeCriteria(type);
+
+    return oul::opencl()->getDevices(criteria);
+}
+
+std::vector<cl::Device> getCPUDevices(){
+    return getDevices(oul::DEVICE_TYPE_CPU);
+}
+
+std::vector<cl::Device> getGPUDevices(){
+    return getDevices(oul::DEVICE_TYPE_GPU);
+}
+
+std::vector<cl::Device> getAllDevices(){
+    return getDevices(oul::DEVICE_TYPE_ANY);
+}
+//------------------------------------------------------------------------------------------------
+//Tests
+TEST_CASE("Can create instance of the manager.","[OpenCLManager]"){
     CHECK(oul::opencl() != NULL);
 }
 
-TEST_CASE("OpenCLManager: Can set debug mode.","[OpenCLManager]"){
+TEST_CASE("Can set debug mode.","[OpenCLManager]"){
     CHECK(oul::opencl()->getDebugMode() == false);
     oul::opencl()->setDebugMode(true);
     CHECK(oul::opencl()->getDebugMode() == true);
 }
 
-TEST_CASE("OpenCLManager: Can create a oul::Context with defaul oul::DeviceCriteria.","[OpenCLManager]"){
+TEST_CASE("Can create a oul::Context with defaul oul::DeviceCriteria.","[OpenCLManager]"){
     oul::DeviceCriteria criteria;
 
     CHECK_NOTHROW(oul::opencl()->createContext(criteria));
 }
 
-TEST_CASE("OpenCLManager: OpenCL platforms installed.","[OpenCLManager]"){
+TEST_CASE("OpenCL platform(s) installed.","[OpenCLManager]"){
     oul::DeviceCriteria criteria;
 
     CHECK(oul::opencl()->getPlatforms(oul::DEVICE_PLATFORM_ANY).size() != 0);
 }
 
-TEST_CASE("OpenCLManager: OpenCL device(s) available.","[OpenCLManager]"){
-    oul::DeviceCriteria criteria;
-    criteria.setTypeCriteria(oul::DEVICE_TYPE_ANY);
-
-    CHECK(oul::opencl()->getDevices(criteria).size() != 0);
+TEST_CASE("OpenCL device(s) available.","[OpenCLManager]"){
+    CHECK(getAllDevices().size() != 0);
 }
 
-TEST_CASE("OpenCLManager: OpenCL CPU device(s) available.","[OpenCLManager]"){
-    oul::DeviceCriteria criteria;
-    criteria.setTypeCriteria(oul::DEVICE_TYPE_CPU);
-
-    CHECK(oul::opencl()->getDevices(criteria).size() != 0);
+TEST_CASE("OpenCL CPU device(s) available.","[OpenCLManager]"){
+    CHECK(getCPUDevices().size() != 0);
 }
 
-TEST_CASE("OpenCLManager: OpenCL GPU device(s) available.","[OpenCLManager]"){
-    oul::DeviceCriteria criteria;
-    criteria.setTypeCriteria(oul::DEVICE_TYPE_CPU);
-
-    CHECK(oul::opencl()->getDevices(criteria).size() != 0);
+TEST_CASE("OpenCL GPU device(s) available.","[OpenCLManager]"){
+    CHECK(getGPUDevices().size() != 0);
 }
 
-TEST_CASE("DeviceCriteria: Default construction gives expected values.","[DeviceCriteria]"){
+//This test fails on Apple
+TEST_CASE("At least one OpenCL device has OpenGL interop capability.","[OpenCLManage][OpenGL]"){
+    bool foundOpenGLInteropCapableDevice  = false;
+
+    std::vector<cl::Device> devices = getAllDevices();
+    for(unsigned int i=0; i< devices.size(); i++){
+        foundOpenGLInteropCapableDevice = foundOpenGLInteropCapableDevice or oul::OpenCLManager::deviceHasOpenGLInteropCapability(devices[i]);
+    }
+
+    CHECK(foundOpenGLInteropCapableDevice);
+}
+
+TEST_CASE("Default construction gives expected values.","[DeviceCriteria]"){
     oul::DeviceCriteria criteria;
 
     CHECK(criteria.getTypeCriteria() == oul::DEVICE_TYPE_ANY);
